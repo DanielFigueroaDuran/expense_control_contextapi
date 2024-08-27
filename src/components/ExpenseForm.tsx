e61b5f21ceeb286469ad6b23276d349cf11d5a61
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-08-27 19:26:17 +0200

    adding a calendar

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,4 +1,11 @@
 import { categories } from "../data/categories"
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const ExpenseForm = () => {
       return (
@@ -61,6 +68,18 @@ const ExpenseForm = () => {
                                     </option>
                               ))}
                         </select>
+                  </div>
+
+                  <div className="flex flex-col gap-2">
+                        <label
+                              htmlFor="amount"
+                              className="text-xl"
+                        >
+                              Fecha Gasto:
+                        </label>
+                        <DatePicker
+                              className="bg-slate-100 p-2 border-0"
+                        />
                   </div>
 
                   <input
