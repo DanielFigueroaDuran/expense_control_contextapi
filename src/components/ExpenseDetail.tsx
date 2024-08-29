f1e7856d749c0a85eb57f0d5d73a982c5896fd32
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-08-29 20:52:22 +0200

    Adding Swipe Interaction to Expenses for Edit and Delete

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,4 +1,12 @@
 import { useMemo } from "react"
+import {
+      LeadingActions,
+      SwipeableList,
+      SwipeableListItem,
+      SwipeAction,
+      TrailingActions,
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
 import { formatDate } from "../helpers"
 import { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
@@ -11,27 +19,56 @@ type ExpenseDetailProps = {
 const ExpenseDetail = ({ expense }: ExpenseDetailProps) => {
 
       const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0],
-            [expense])
+            [expense]);
+
+      const leadingActions = () => (
+            <LeadingActions>
+                  <SwipeAction onClick={() => { }}>
+                        Actualizar
+                  </SwipeAction>
+            </LeadingActions>
+      );
+
+      const trailingActions = () => (
+            <TrailingActions>
+                  <SwipeAction
+                        destructive={true}
+                        onClick={() => { }}
+                  >
+                        Eliminar
+                  </SwipeAction>
+            </TrailingActions>
+      );
 
       return (
-            <div className="flex gap-5 items-center bg-white shadow-lg p-10 w-full border-b border-gray-200 ">
-                  <div className="">
-                        <img
-                              className="w-20"
-                              src={`/icono_${categoryInfo.icon}.svg`}
-                              alt="icon"
-                        />
-                  </div>
 
-                  <div className="flex-1 space-y-2">
-                        <p className="text-sm font-bold uppercase text slate-500">{categoryInfo.name}</p>
-                        <p>{expense.expenseName}</p>
-                        <p>{formatDate(expense.date!.toString())}</p>
-                  </div>
-                  <AmountDisplay
-                        amount={expense.amount}
-                  />
-            </div>
+            <SwipeableList>
+                  <SwipeableListItem
+                        maxSwipe={30}
+                        leadingActions={leadingActions()}
+                        trailingActions={trailingActions()}
+                  >
+                        <div className="flex gap-5 items-center bg-white shadow-lg p-10 w-full border-b border-gray-200 ">
+                              <div className="">
+                                    <img
+                                          className="w-20"
+                                          src={`/icono_${categoryInfo.icon}.svg`}
+                                          alt="icon"
+                                    />
+                              </div>
+
+                              <div className="flex-1 space-y-2">
+                                    <p className="text-sm font-bold uppercase text slate-500">{categoryInfo.name}</p>
+                                    <p>{expense.expenseName}</p>
+                                    <p>{formatDate(expense.date!.toString())}</p>
+                              </div>
+                              <AmountDisplay
+                                    amount={expense.amount}
+                              />
+                        </div>
+                  </SwipeableListItem>
+            </SwipeableList>
+
       )
 }
 
