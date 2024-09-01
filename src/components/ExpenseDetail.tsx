363c32c33d03e0f50dfef69fdebb1c1364146b56
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-09-01 17:56:33 +0200

    Selecting the Expense to Edit when we execute Swipe in the other direction

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -26,7 +26,7 @@ const ExpenseDetail = ({ expense }: ExpenseDetailProps) => {
 
       const leadingActions = () => (
             <LeadingActions>
-                  <SwipeAction onClick={() => { }}>
+                  <SwipeAction onClick={() => dispatch({ type: 'get-expense-by-id', payload: { id: expense.id } })}>
                         Actualizar
                   </SwipeAction>
             </LeadingActions>
--- src/reducer/budget-reducer.ts
+++ src/reducer/budget-reducer.ts
@@ -6,12 +6,14 @@ export type BudgetActions =
       { type: 'show-modal' } |
       { type: 'close-modal' } |
       { type: 'add-expense', payload: { expense: DrafExpense } } |
-      { type: 'remove-expense', payload: { id: Expense['id'] } }
+      { type: 'remove-expense', payload: { id: Expense['id'] } } |
+      { type: 'get-expense-by-id', payload: { id: Expense['id'] } }
 
 export type Budgetstate = {
       budget: number,
       modal: boolean,
-      expenses: Expense[]
+      expenses: Expense[],
+      editingId: Expense['id']
 }
 
 const createExpense = (draftExpense: DrafExpense): Expense => {
@@ -24,7 +26,8 @@ const createExpense = (draftExpense: DrafExpense): Expense => {
 export const initialState: Budgetstate = {
       budget: 0,
       modal: false,
-      expenses: []
+      expenses: [],
+      editingId: ''
 }
 
 export const budgetReducer = (
@@ -73,6 +76,14 @@ export const budgetReducer = (
             return {
                   ...state,
                   expenses: state.expenses.filter(expense => expense.id !== action.payload.id)
+            }
+      }
+
+      if (action.type === 'get-expense-by-id') {
+            return {
+                  ...state,
+                  editingId: action.payload.id,
+                  modal: true
             }
       }
 
